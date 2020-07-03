bbd9f0205ebac4f26d661ac4fbe0ac198fe74758
Cole Michaels <cmichaelsd@gmail.com>
2020-07-03 11:42:57 -0700

    Initial Commit

---
+++ BrickBreak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrickBreak
+//
+//  Created by Cole Michaels on 7/3/20.
+//  Copyright © 2020 cmichaelsd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
